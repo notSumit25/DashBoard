3d361c18831b6cb331cd3213d100b9452a3346ad
Sumit <sumitgarg380@gmail.com>
2023-07-31 22:43:38 +0530

    Updated Route

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -3,7 +3,6 @@ import { mongooseConnect } from "@/lib/mongoose";
 
 export default async function handler(req, res) {
   const { method } = req;
-
   if (method === "POST") {
     try {
       await mongooseConnect();
@@ -21,6 +20,6 @@ export default async function handler(req, res) {
       return res.status(500).json({ error: "Failed to create product" });
     }
   } else {
-    return res.status(405).json({ error: "Method Not Allowed" });
+    return res.status(405).json({ error: "Method Not Allowed." });
   }
 };
