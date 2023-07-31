633a25309fee256fe20e9afa337056c28976323f
Sumit <sumitgarg380@gmail.com>
2023-07-31 22:31:53 +0530

    asdadsa

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -1,21 +1,26 @@
-
-import mongoose from "mongoose";
 import { Product } from "@/models/Products";
 import { mongooseConnect } from "@/lib/mongoose";
 
-export default async function handlePOST(req,res) {
-    const {method} = req;
-    await mongooseConnect();
-    if(method==='POST'){
-        try {
-        const {title,description,price} = req.body;
-        const newProd = await Product.create({
-            title,description,price
-        })
-        res.json(newProd);    
-        } catch (error) {
-            console.log(error);    
-        }
-        
+export default async function handler(req, res) {
+  const { method } = req;
+
+  if (method === "POST") {
+    try {
+      await mongooseConnect();
+
+      const { title, description, price } = req.body;
+      const newProd = await Product.create({
+        title,
+        description,
+        price,
+      });
+
+      return res.status(201).json(newProd);
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Failed to create product" });
     }
+  } else {
+    return res.status(405).json({ error: "Method Not Allowed" });
+  }
 };
