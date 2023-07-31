9738ef4cda62357e75423f0bc549b865d27a4175
Sumit <sumitgarg380@gmail.com>
2023-07-31 21:01:17 +0530

    Made changes

--- app/api/products/route.js
+++ app/api/products/route.js
@@ -3,14 +3,19 @@ import mongoose from "mongoose";
 import { Product } from "@/models/Products";
 import { mongooseConnect } from "@/lib/mongoose";
 
-export default async function POST(req,res) {
+export default async function handlePOST(req,res) {
     const {method} = req;
     await mongooseConnect();
     if(method==='POST'){
+        try {
         const {title,description,price} = req.body;
         const newProd = await Product.create({
             title,description,price
         })
-        res.json(newProd);
+        res.json(newProd);    
+        } catch (error) {
+            console.log(error);    
+        }
+        
     }
 };
